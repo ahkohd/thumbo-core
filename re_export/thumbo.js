dafac38e653f8cd0a8aa19e5d5a97735237f9647
ahkohd <victor.olorunbunmi@gmail.com>
2021-10-27 18:49:09 +0100

    refactor: remove

--- re_export/thumbo.js
+++
@@ -1,8 +0,0 @@
-import * as wasm from "./thumbo_core_bg.wasm";
-export * from "./thumbo_core_bg.js";
-
-export const memory = wasm.memory;
-
-export const readFromMemory = (result) => {
-    return new Uint8Array(memory.buffer, result.img_ptr, result.img_size);
-};
